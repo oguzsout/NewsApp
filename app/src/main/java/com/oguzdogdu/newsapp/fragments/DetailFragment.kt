bd9a80ef7ba9f57279fa2cd0acee69db2cd8d755
oguzsout <oguzd494@gmail.com>
2021-10-25 17:44:22 +0300

    Navigation added Fragments added Added the Recyclerview row

---
+++ app/src/main/java/com/oguzdogdu/newsapp/fragments/DetailFragment.kt
@@ -0,0 +1,20 @@
+package com.oguzdogdu.newsapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.oguzdogdu.newsapp.R
+
+
+class DetailFragment : Fragment(R.layout.fragment_detail) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+}
