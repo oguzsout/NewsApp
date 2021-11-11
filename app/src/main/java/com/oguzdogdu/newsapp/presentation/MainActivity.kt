3027bd3e8177b3e8f65b6da58cf530b832ea5a45
Oguzhan Dogdu <oguzd494@gmail.com>
2021-11-11 22:24:17 +0300

    Bottom navigation view has been customized. Search fragment is set.

--- app/src/main/java/com/oguzdogdu/newsapp/presentation/MainActivity.kt
+++ app/src/main/java/com/oguzdogdu/newsapp/presentation/MainActivity.kt
@@ -2,44 +2,24 @@ package com.oguzdogdu.newsapp.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.ismaeldivita.chipnavigation.ChipNavigationBar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.oguzdogdu.newsapp.R
 import com.oguzdogdu.newsapp.databinding.ActivityMainBinding
-import com.oguzdogdu.newsapp.presentation.fragments.newsfragment.NewsFragment
-import com.oguzdogdu.newsapp.presentation.fragments.searchfragment.SearchFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        setSupportActionBar(
-            binding.toolbar
-        )
-        supportActionBar?.setDisplayShowTitleEnabled(false)
-        binding.bottomNavBar.setOnItemSelectedListener {
-            when (it) {
-                R.id.newsFragmentBottom ->{
-                    NewsFragment()
-                }
-                R.id.searchFragmentBottom -> {
-
-                    SearchFragment()
-                }
-
-
-            }
-        }
-        binding.bottomNavBar.apply {
-            setMenuOrientation(ChipNavigationBar.MenuOrientation.HORIZONTAL)
-            setMenuResource(R.menu.nav_menu)
-        }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
 
     }
-
-
 }
