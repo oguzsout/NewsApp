5f8f2e8354f8bc39c6c4e9cea3622fdb7658c17f
Oguzhan Dogdu <oguzd494@gmail.com>
2021-12-13 19:09:54 +0300

    BaseFragment.kt was implemetationed succesfully to DetailFragment.kt

--- app/src/main/java/com/oguzdogdu/newsapp/presentation/fragments/detailfragment/DetailFragment.kt
+++ app/src/main/java/com/oguzdogdu/newsapp/presentation/fragments/detailfragment/DetailFragment.kt
@@ -1,37 +1,21 @@
 package com.oguzdogdu.newsapp.presentation.fragments.detailfragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
-import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import coil.load
 import coil.transform.RoundedCornersTransformation
-import com.oguzdogdu.newsapp.R
 import com.oguzdogdu.newsapp.databinding.FragmentDetailBinding
+import com.oguzdogdu.newsapp.presentation.fragments.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class DetailFragment : Fragment(R.layout.fragment_detail) {
-
-    private var _binding: FragmentDetailBinding? = null
-    private val binding get() = _binding!!
+class DetailFragment : BaseFragment<FragmentDetailBinding>(FragmentDetailBinding::inflate) {
 
     private val args: DetailFragmentArgs by navArgs()
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentDetailBinding.inflate(
-            inflater, container, false
-        )
-        return binding.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         backStack()
@@ -79,11 +63,5 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
         }
 
         requireActivity().onBackPressedDispatcher.addCallback(callBack)
-    }
-
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
